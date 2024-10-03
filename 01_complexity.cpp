30e1187b6dfab38c9366b31b5faf3b893f355dbf
Sthefanie <sthefaniepasso@gmail.com>
2024-10-03 08:39:12 -0500

    Sort algorithms

---
+++ 01_complexity.cpp
@@ -0,0 +1,12 @@
+/* 
+Complexity of our algorithm:
+Sort algorithms: https://lamfo-unb.github.io/2019/04/21/Sorting-algorithms/
+array -> 6 elements
+Buble Sort (O(n^2) = 6^2 = 36)
+Merge Sort (O(nlogn) = 6log(6) = 4.7)
+
+
+Homework:
+install github
+install terminal bash
+*/
